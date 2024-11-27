a7a435b58672e10f59ba1e95d00df9a62edb99d7
Kshitij Sahdev <85095194+Pr1s0n3r627@users.noreply.github.com>
2024-11-27 14:57:09 +0530

    done

--- my-java-app/src/main/java/com/example/App.java
+++ my-java-app/src/main/java/com/example/App.java
@@ -1,13 +1,9 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "\n\nif you see this the job is complete\n\n" );
     }
 }
